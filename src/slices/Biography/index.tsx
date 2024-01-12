8397340aede0f0ef1dd4be1002b4fa7a2561c7b5
Rabin Chakraborty <rabin747809@gmail.com>
2024-01-12 15:44:23 +0530

    Added tailwind typography package and updated the tailwind config

--- src/slices/Biography/index.tsx
+++ src/slices/Biography/index.tsx
@@ -16,7 +16,7 @@ const Biography = ({ slice }: BiographyProps): JSX.Element => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <div className='grid gap-y-6  gap-x-8 md:grid-cols-[2fr,1fr] '></div>
+      <div className='grid gap-y-6 gap-x-8 md:grid-cols-[2fr,1fr]'></div>
     </Bounded>
   );
 };
