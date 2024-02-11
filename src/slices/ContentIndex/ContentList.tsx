45be07106c76c24c26c3ff378cde4db7107e0c44
Rabin Chakraborty <rabin747809@gmail.com>
2024-02-11 16:27:28 +0530

    adding blog post page

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -4,9 +4,9 @@ import { Content } from '@prismicio/client';
 
 type ContentListProps = {
   items: Content.BlogPostDocument[] | Content.ProjectDocument[];
-  contentType: Content.BlogPostIndexSlice['primary']['content_type'];
-  fallbackItemImage: Content.BlogPostIndexSlice['primary']['fallback_item_image'];
-  viewMoreText: Content.BlogPostIndexSlice['primary']['view_more_text'];
+  contentType: Content.ContentIndexSlice['primary']['content_type'];
+  fallbackItemImage: Content.ContentIndexSlice['primary']['fallback_item_image'];
+  viewMoreText: Content.ContentIndexSlice['primary']['view_more_text'];
 };
 
 const ContentList = ({
