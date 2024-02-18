99833e5884465c6a59433eac561a538d097342c3
Rabin Chakraborty <rabin747809@gmail.com>
2024-02-18 12:34:56 +0530

    adding blog and project  post page

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import { Content, isFilled } from '@prismicio/client';
 import { MdArrowOutward } from 'react-icons/md';
 import Link from 'next/link';
+import { components } from './../index';
 
 type ContentListProps = {
   items: Content.BlogPostDocument[] | Content.ProjectDocument[];
@@ -17,35 +18,40 @@ const ContentList = ({
   fallbackItemImage,
   viewMoreText = 'Read More',
 }: ContentListProps) => {
+  const component = useRef(null);
   const urlPrefix = contentType === 'Blog' ? '/blog' : '/project';
   return (
-    <ul className='grid border-b border-b-slate-100'>
-      {items.map((item, index) => (
-        <>
-          {isFilled.keyText(item.data.title) && (
-            <li key={index} className='list-item opacity-0f'>
-              <Link
-                href={urlPrefix + '/' + item.uid}
-                className='flex flex-col justify-between border-t border-t-slate-100 text-slate-200 md:flex-row'
-                aria-label={item.data.title}
-              >
-                <div className='flex flex-col'>
-                  <span className='text-3xl font-bold'>{item.data.title}</span>
-                  <div className='flex gap-3 text-yellow-400 text-lg font-bold'>
-                    {item.tags.map((tag, index) => (
-                      <span key={index}>{tag}</span>
-                    ))}
+    <div ref={component}>
+      <ul className='grid border-b border-b-slate-100'>
+        {items.map((item, index) => (
+          <>
+            {isFilled.keyText(item.data.title) && (
+              <li key={index} className='list-item opacity-0f'>
+                <Link
+                  href={urlPrefix + '/' + item.uid}
+                  className='flex flex-col justify-between border-t border-t-slate-100 text-slate-200 md:flex-row'
+                  aria-label={item.data.title}
+                >
+                  <div className='flex flex-col'>
+                    <span className='text-3xl font-bold'>
+                      {item.data.title}
+                    </span>
+                    <div className='flex gap-3 text-yellow-400 text-lg font-bold'>
+                      {item.tags.map((tag, index) => (
+                        <span key={index}>{tag}</span>
+                      ))}
+                    </div>
                   </div>
-                </div>
-                <span className='ml-auto flex items-center gap-2 text-xl font-medium md:ml-0'>
-                  {viewMoreText} <MdArrowOutward />
-                </span>
-              </Link>
-            </li>
-          )}
-        </>
-      ))}
-    </ul>
+                  <span className='ml-auto flex items-center gap-2 text-xl font-medium md:ml-0'>
+                    {viewMoreText} <MdArrowOutward />
+                  </span>
+                </Link>
+              </li>
+            )}
+          </>
+        ))}
+      </ul>
+    </div>
   );
 };
 
