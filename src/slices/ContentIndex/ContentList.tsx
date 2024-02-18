7d6ffc9fce9b02b804aca82ef6928b1a77c72a8a
Rabin Chakraborty <rabin747809@gmail.com>
2024-02-18 12:34:56 +0530

    adding blog and project  post page

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React, { useState, useEffect } from 'react';
-import { Content } from '@prismicio/client';
+import { Content, isFilled } from '@prismicio/client';
 import { MdArrowOutward } from 'react-icons/md';
 import Link from 'next/link';
 
@@ -21,24 +21,29 @@ const ContentList = ({
   return (
     <ul className='grid border-b border-b-slate-100'>
       {items.map((item, index) => (
-        <li key={index} className='list-item opacity-0f'>
-          <Link
-            href={urlPrefix + '/' + item.uid}
-            className='flex flex-col justify-between border-t border-t-slate-100 text-slate-200 md:flex-row'
-          >
-            <div className='flex flex-col'>
-              <span className='text-3xl font-bold'>{item.data.title}</span>
-              <div className='flex gap-3 text-yellow-400 text-lg font-bold'>
-                {item.tags.map((tag, index) => (
-                  <span key={index}>{tag}</span>
-                ))}
-              </div>
-            </div>
-            <span className='ml-auto flex items-center gap-2 text-xl font-medium md:ml-0'>
-              {viewMoreText} <MdArrowOutward />
-            </span>
-          </Link>
-        </li>
+        <>
+          {isFilled.keyText(item.data.title) && (
+            <li key={index} className='list-item opacity-0f'>
+              <Link
+                href={urlPrefix + '/' + item.uid}
+                className='flex flex-col justify-between border-t border-t-slate-100 text-slate-200 md:flex-row'
+                aria-label={item.data.title}
+              >
+                <div className='flex flex-col'>
+                  <span className='text-3xl font-bold'>{item.data.title}</span>
+                  <div className='flex gap-3 text-yellow-400 text-lg font-bold'>
+                    {item.tags.map((tag, index) => (
+                      <span key={index}>{tag}</span>
+                    ))}
+                  </div>
+                </div>
+                <span className='ml-auto flex items-center gap-2 text-xl font-medium md:ml-0'>
+                  {viewMoreText} <MdArrowOutward />
+                </span>
+              </Link>
+            </li>
+          )}
+        </>
       ))}
     </ul>
   );
