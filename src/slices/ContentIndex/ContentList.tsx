f04a3034fb5dd59998a30d7c0d597ea0fbc46821
Rabin Chakraborty <rabin747809@gmail.com>
2024-02-27 12:34:56 +0530

    adding blog and project  post page

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -4,6 +4,7 @@ import { Content, asImageSrc, isFilled } from '@prismicio/client';
 import { MdArrowOutward } from 'react-icons/md';
 import Link from 'next/link';
 import { components } from './../index';
+import gsap from 'gsap';
 
 type ContentListProps = {
   items: Content.BlogPostDocument[] | Content.ProjectDocument[];
@@ -18,15 +19,38 @@ const ContentList = ({
   fallbackItemImage,
   viewMoreText = 'Read More',
 }: ContentListProps) => {
+  const component = useRef(null);
+  const revealRef = useRef(null);
+  const [currentItem, SetCurrentItem] = useState<null | number>(null);
   const lastMousePos = useRef({ x: 0, y: 0 });
   useEffect(() => {
     const handleMouseMove = (e: MouseEvent) => {
       const mousePos = { x: e.clientX, y: e.clientY + window.scrollY };
-    };
-  });
+      const speed = Math.sqrt(Math.pow(mousePos.x - lastMousePos.current.x, 2));
 
-  const component = useRef(null);
-  const [currentItem, SetCurrentItem] = useState<null | number>(null);
+      let ctx = gsap.context(() => {
+        if (currentItem !== null) {
+          const maxY = window.scrollY + window.innerHeight - 350;
+          const maxX = window.innerWidth - 250;
+
+          gsap.to(revealRef.current, {
+            x: gsap.utils.clamp(0, maxX, mousePos.x - 110),
+            y: gsap.utils.clamp(0, maxY, mousePos.y - 160),
+            rotation: speed * (mousePos.x > lastMousePos.current.x ? 1 : -1),
+            ease: 'back.out(2)',
+            duration: 1.3,
+          });
+        }
+        lastMousePos.current = mousePos;
+        return () => ctx.revert();
+      }, component);
+    };
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, [currentItem]);
+
   const urlPrefix = contentType === 'Blog' ? '/blog' : '/project';
   const contentImages = items.map((item) => {
     const image = isFilled.image(item.data.hover_image)
@@ -86,6 +110,7 @@ const ContentList = ({
       </ul>
       {/* Hover Eliment */}
       <div
+        ref={revealRef}
         className='hover-reveal pointer-events-none absolute left-0 top-0 -z-10 h-[320px] w-[220px] rounded-lg bg-over bg-center opacity-0f transition-[background] duration-300 '
         style={{
           backgroundImage:
