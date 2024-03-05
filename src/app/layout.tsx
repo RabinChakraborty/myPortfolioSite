2b6329c8225fc3fc3b0880a6a7a4589dd7ba4d99
Rabin Chakraborty <rabin747809@gmail.com>
2024-03-05 12:34:56 +0530

    adding footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Urbanist } from 'next/font/google';
 import './globals.css';
 import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 const urbanist = Urbanist({ subsets: ['latin'] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={urbanist.className}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
