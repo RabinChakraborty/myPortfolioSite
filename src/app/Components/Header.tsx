43c01b7cacafd205bd3fa0dd2007acac3c0a4fdd
Rabin Chakraborty <rabin747809@gmail.com>
2024-03-05 12:34:56 +0530

    adding navbar

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -2,28 +2,14 @@ import React from 'react';
 import { createClient } from '@/prismicio';
 import Link from 'next/link';
 import { PrismicNextLink } from '@prismicio/next';
+import NavBar from './NavBar';
 
 export default async function Header() {
   const client = createClient();
   const settings = await client.getSingle('settings');
   return (
     <header className='top-0 z-50 mx-auto max-w-7xl md:sticky md:top-4'>
-      <nav>
-        <ul>
-          <li>
-            <Link href='/' aria-label='Home Page'>
-              {settings.data.name}
-            </Link>
-          </li>
-          {settings.data.nav_item.map(({ link, label }, index) => (
-            <li key={index}>
-              <PrismicNextLink field={link}>{label}</PrismicNextLink>
-            </li>
-          ))}
-
-          <li></li>
-        </ul>
-      </nav>
+      <NavBar settings={settings} />
     </header>
   );
 }
