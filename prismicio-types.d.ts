f7819571f76340157e8834d452571adacfd28e7c
Rabin Chakraborty <rabin747809@gmail.com>
2024-03-12 14:34:56 +0530

    adding projects and blogs

--- prismicio-types.d.ts
+++ prismicio-types.d.ts
@@ -723,6 +723,21 @@ type HeroSliceVariation = HeroSliceDefault;
 export type HeroSlice = prismic.SharedSlice<"hero", HeroSliceVariation>;
 
 /**
+ * Primary content in *ImageBlock → Primary*
+ */
+export interface ImageBlockSliceDefaultPrimary {
+  /**
+   * Image field in *ImageBlock → Primary*
+   *
+   * - **Field Type**: Image
+   * - **Placeholder**: *None*
+   * - **API ID Path**: image_block.primary.image
+   * - **Documentation**: https://prismic.io/docs/field#image
+   */
+  image: prismic.ImageField<never>;
+}
+
+/**
  * Default variation for ImageBlock Slice
  *
  * - **API ID**: `default`
@@ -731,7 +746,7 @@ export type HeroSlice = prismic.SharedSlice<"hero", HeroSliceVariation>;
  */
 export type ImageBlockSliceDefault = prismic.SharedSliceVariation<
   "default",
-  Record<string, never>,
+  Simplify<ImageBlockSliceDefaultPrimary>,
   never
 >;
 
@@ -906,6 +921,7 @@ declare module "@prismicio/client" {
       HeroSliceVariation,
       HeroSliceDefault,
       ImageBlockSlice,
+      ImageBlockSliceDefaultPrimary,
       ImageBlockSliceVariation,
       ImageBlockSliceDefault,
       TechListSlice,
